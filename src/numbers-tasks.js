b2c89cf5aa04a85cc59a4eb555f04252d04e0afa
Serj Lyakhov <serjml@list.ru>
2024-11-10 19:13:04 +0300

    feat: complete the getCountOfOddNumbers task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -627,8 +627,12 @@ function getHypotenuse(a, b) {
  * 10 => 5
  * 15 => 8
  */
-function getCountOfOddNumbers(/* number */) {
-  throw new Error('Not implemented');
+function getCountOfOddNumbers(number) {
+  let count = 0;
+  for (let i = 1; i <= Math.abs(number); i += 2) {
+    count += 1;
+  }
+  return count;
 }
 
 module.exports = {
